957116eb44e9f6fcc140fb838535f48852260b12
Mmerchan450 <Mmerchan450@gmail.com>
2025-07-21 12:42:09 +0200

    Proyecto Cypress Basico

---
+++ CypressTodomvc/cypress/e2e/todomvc.cy.js
@@ -0,0 +1,18 @@
+describe('Visitar TodoMVC React Dist', () => {
+    it('Carga la página correctamente', () => {
+      cy.visit('https://todomvc.com/examples/react/dist/')
+    })
+    it('Escribe tarea "hacer la compra"', () => {
+        cy.visit('https://todomvc.com/examples/react/dist/')
+        cy.get('[data-testid="text-input"]').type("hacer la compra{enter}") 
+        
+      })
+
+      it('Marcar tarea como completada', () => {
+        cy.visit('https://todomvc.com/examples/react/dist/')
+        cy.get('[data-testid="text-input"]').type("hacer la compra{enter}") 
+        cy.get('[data-testid="todo-item-toggle"]').click()
+        cy.get('.todo-list li').first().should('have.class', 'completed')
+
+        })
+  })
