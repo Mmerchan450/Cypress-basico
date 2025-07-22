180dee8fc864ade1295feccdbecfed098f6a70b8
Mmerchan450 <Mmerchan450@gmail.com>
2025-07-22 10:32:06 +0200

    añado nuevas pruebas cypress

--- CypressTodomvc/cypress/e2e/todomvc.cy.js
+++ CypressTodomvc/cypress/e2e/todomvc.cy.js
@@ -1,18 +1,28 @@
 describe('Visitar TodoMVC React Dist', () => {
-    it('Carga la página correctamente', () => {
+
+  beforeEach(() => {
+    cy.visit('https://todomvc.com/examples/react/dist/')
+  })
+  it('Carga la página correctamente', () => {
       cy.visit('https://todomvc.com/examples/react/dist/')
-    })
-    it('Escribe tarea "hacer la compra"', () => {
-        cy.visit('https://todomvc.com/examples/react/dist/')
-        cy.get('[data-testid="text-input"]').type("hacer la compra{enter}") 
-        
-      })
+  })
 
-      it('Marcar tarea como completada', () => {
-        cy.visit('https://todomvc.com/examples/react/dist/')
-        cy.get('[data-testid="text-input"]').type("hacer la compra{enter}") 
-        cy.get('[data-testid="todo-item-toggle"]').click()
-        cy.get('.todo-list li').first().should('have.class', 'completed')
+  it('Escribe tarea "hacer la compra"', () => {
+      cy.visit('https://todomvc.com/examples/react/dist/')
+      cy.get('[data-testid="text-input"]').type("hacer la compra{enter}") 
+  })
 
-        })
+  it('Marcar tarea como completada', () => {
+      cy.visit('https://todomvc.com/examples/react/dist/')
+      cy.get('[data-testid="text-input"]').type("hacer la compra{enter}") 
+      cy.get('[data-testid="todo-item-toggle"]').click()
+      cy.get('.todo-list li').first().should('have.class', 'completed')
+  })
+ 
+  it('Eliminar tareas completadas', () => {
+    cy.get('.clear-completed').click()
+})
+
+
+
   })
